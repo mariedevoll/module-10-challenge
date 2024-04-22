c7a15cfdbd4800e2639c5a6487a030620a99b581
mariedevoll <marie.devoll@gmail.com>
2024-04-22 12:57:45 -0400

    before class work

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,12 +1,55 @@
 // const { describe } = require("yargs")
 const { Square, Triangle, Circle } = require("../lib/shapes")
 
-describe("Square", () => {
+describe("Circle", () => {
     test("should render svg for a green circle element", () => {
-
+        const expectedSVG = `<circle cs="150" cy="100" r="80" fill="red"/>`
+        const circle = new Circle();
+        circle.setColor("red");
+        const actualSVG = circle.render();
+        expectedSVG(actualSVG).toEqual(expectedSVG);
     })
 
 test("should accept a fillColor input", () => {
     const expectedSVG = `<circle cs="150" cy="100" r="80" fill="purple"/>`
+    const circle = new Circle()
+    // setter method to purple
+    circle.setColor("purple")
+    const actualSVG = circle.render();
+    expectedSVG(actualSVG).toEqual(expectedSVG)
+    })
 })
+//
+describe("Triangle", () => {
+    test("should render svg for orange polygon element", () => {
+        const expectedSVG = `<polygon points="150, 18, 244, 182, 56, 182" fill="teal"/>`
+        const triangle = new Triangle()
+        triangle.setColor("teal")
+        const actualSVG = triangle.render()
+        expectedSVG(actualSVG).toEqual(expectedSVG)
+        
+    })
+    test("should accept a fillColor input", () => {
+        const expectedSVG = `<polygon points="150, 18, 244, 182, 56, 182" fill="teal"/>`
+        const triangle = new Triangle()
+        triangle.setColor("red")
+        const actualSVG = triangle.render()
+        expectedSVG(actualSVG).toEqual(expectedSVG)
+    })
+})
+//
+describe("Square", () => {
+    test("should render svg for yellow polygon element", () => {
+        const expectedSVG = `<rect x="90" y="40" width="120" height="120" fill="yellow"/>`
+        const triangle = new Square()
+        triangle.setColor("yellow")
+
+    })
+    test("should accept a fillColor input", () => {
+        const expectedSVG = `<rect x="90" y="40" width="120" height="120" fill="yellow"/>`
+        const triangle = new Triangle()
+        triangle.setColor("black")
+        const actualSVG = triangle.render()
+        expectedSVG(actualSVG).toEqual(expectedSVG)
+    })
 })
---
+++ tests/svg.test.js
@@ -0,0 +1,39 @@
+const SVG = require("../lib/svg");
+const {Square} = require("../lib/shapes");
+
+test("should render a 300x200 svg element", () => {
+    const expectedSVG = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg></sgv>`;
+    const svg = new SVG();
+    const actualSVG = svg.render();
+    expect(actualSVG).toEqual(expectedSVG);
+})
+
+test("should append text element", () => {
+    const expectedSVG = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg><text x="150" y="125" font-size="60" text-anchor="middle" fill="red">A</text></svg>`;
+    const svg = new SVG();
+    svg.setText("A", "red")
+    expect(svg.render()).toEqual(expectedSVG);
+})
+
+test("should set text message and color", () => {
+    const expectedSVG = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg><text x="150" y="125" font-size="60" text-anchor="middle" fill="orange">SVG</text></svg>`;
+    const svg = new SVG();
+    svg.setText("SVG", "orange")
+    expect(svg.render()).toEqual(expectedSVG);
+})
+
+test("should throw error if text exceeds 3 characters", () => {
+    const expectedError = new Error("Text must not exceed 3 characters")
+    const svg = new SVG();
+    const actualResult = () => svg.setText("HELLOO", "#333")
+
+    expect(actualResult).toThrow(expectedSVG);
+})
+
+test("should include a shape", () => {
+    const expectedSVG = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg><rect x="90" y="40" width="120" height="120" fill="yellow"/><text x="150" y="125" font-size="60" text-anchor="middle" fill="red">A</text></svg>`;
+    const svg = new SVG();
+    svg.setText("SVG", "orange")
+
+    expect(actualResult).toEqual(expectedSVG);
+})
