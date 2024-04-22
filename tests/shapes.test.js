79f15c47de4d0659d96ca8174febd2a5c1989dcd
mariedevoll <marie.devoll@gmail.com>
2024-04-22 12:03:56 -0400

    testing

---
+++ tests/shapes.test.js
@@ -0,0 +1,12 @@
+// const { describe } = require("yargs")
+const { Square, Triangle, Circle } = require("../lib/shapes")
+
+describe("Square", () => {
+    test("should render svg for a green circle element", () => {
+
+    })
+
+test("should accept a fillColor input", () => {
+    const expectedSVG = `<circle cs="150" cy="100" r="80" fill="purple"/>`
+})
+})
