62a4ade6ad16a7ea5f82561360d073994f5a6bfa
mariedevoll <marie.devoll@gmail.com>
2024-04-22 21:30:07 -0400

    calling it a night

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -5,50 +5,50 @@ describe("Circle", () => {
     test("should render svg for a green circle element", () => {
         const expectedSVG = `<circle cx ="150" cy="100" r="80" fill="green"/>`
         const circle = new Circle();
-        circle.setColor("green");
+        circle.setColor("green")
         const actualSVG = circle.render();
-        expectedSVG(actualSVG).toEqual(expectedSVG);
+        expectedSVG(actualSVG).toEqual(expectedSVG)
     })
  
 test("should accept a fillColor input", () => {
     const expectedSVG = `<circle cs="150" cy="100" r="80" fill="green"/>`
-    const circle = new Circle()
-    circle.setColor("green")
+    const circle = new Circle();
+    circle.setColor("green");
     const actualSVG = circle.render();
-    expectedSVG(actualSVG).toEqual(expectedSVG)
+    expectedSVG(actualSVG).toEqual(expectedSVG);
     })
 })
 //triangle
 describe("Triangle", () => {
     test("should render svg for orange polygon element", () => {
         const expectedSVG = `<polygon points="150, 18, 244, 182, 56, 182" fill="orange"/>`
-        const triangle = new Triangle()
-        triangle.setColor("orange")
-        const actualSVG = triangle.render()
-        expectedSVG(actualSVG).toEqual(expectedSVG)
+        const triangle = new Triangle();
+        triangle.setColor("orange");
+        const actualSVG = triangle.render();
+        expectedSVG(actualSVG).toEqual(expectedSVG);
         
     })
     test("should accept a fillColor input", () => {
         const expectedSVG = `<polygon points="150, 18, 244, 182, 56, 182" fill="orange"/>`
-        const triangle = new Triangle()
-        triangle.setColor("orange")
-        const actualSVG = triangle.render()
-        expectedSVG(actualSVG).toEqual(expectedSVG)
+        const triangle = new Triangle();
+        triangle.setColor("orange");
+        const actualSVG = triangle.render();
+        expectedSVG(actualSVG).toEqual(expectedSVG);
     })
 })
 //square
 describe("Square", () => {
     test("should render svg for yellow polygon element", () => {
         const expectedSVG = `<rect x="90" y="40" width="120" height="120" fill="yellow"/>`
-        const square = new Square()
-        square.setColor("yellow")
+        const square = new Square();
+        square.setColor("yellow");
 
     })
     test("should accept a fillColor input", () => {
         const expectedSVG = `<rect x="90" y="40" width="120" height="120" fill="yellow"/>`
-        const square = new Square()
-        square.setColor("yellow")
-        const actualSVG = square.render()
-        expectedSVG(actualSVG).toEqual(expectedSVG)
+        const square = new Square();
+        square.setColor("yellow");
+        const actualSVG = square.render();
+        expectedSVG(actualSVG).toEqual(expectedSVG);
     })
-})
+});
--- tests/svg.test.js
+++ tests/svg.test.js
@@ -13,21 +13,21 @@ describe("SVG", () => {
     test("should append text element", () => {
         const expectedSVG = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg><text x="150" y="125" font-size="60" text-anchor="middle" fill="red">A</text></svg>`;
         const svg = new SVG();
-        svg.setText("A", "red")
+        svg.setText("A", "red");
         expect(svg.render()).toEqual(expectedSVG);
     })
 
     test("should set text message and color", () => {
         const expectedSVG = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg><text x="150" y="125" font-size="60" text-anchor="middle" fill="orange">SVG</text></svg>`;
         const svg = new SVG();
-        svg.setText("SVG", "orange")
+        svg.setText("SVG", "orange");
         expect(svg.render()).toEqual(expectedSVG);
     })
 
     test("should throw error if text exceeds 3 characters", () => {
         const expectedError = new Error("Text must not exceed 3 characters")
         const svg = new SVG();
-        const actualResult = () => svg.setText("HELLO", "#333")
+        const actualResult = () => svg.setText("HELLO", "#333");
 
         expect(actualResult).toThrow(expectedError);
     })
@@ -35,7 +35,7 @@ describe("SVG", () => {
     test("should include a shape", () => {
         const expectedSVG = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg><rect x="90" y="40" width="120" height="120" fill="yellow"/><text x="150" y="125" font-size="60" text-anchor="middle" fill="red">SVG</text></svg>`;
         const svg = new SVG();
-        svg.setText("SVG", "red")
+        svg.setText("SVG", "red");
         const square = new Square();
         square.setColor("yellow");
         svg.setShape(square);
